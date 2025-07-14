d8e6f5c8ec7d64ca4865fabd9e90dfa15cf0360a
Mithun Chandramohan <71203585+mithun-cr@users.noreply.github.com>
2025-07-14 19:50:47 +0530

    Update data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -5,7 +5,7 @@ def loaded_csv_data(filepath):
     with open(filepath, 'r',encoding='ISO-8859-1') as file:
         reader = csv.DictReader(file)
         for row in reader:
-            material = row['Material']
+            material = row['Material'].strip()
             mu = float(row['Mu'])
             density = float(row['Density'])
             material_data[material] = (mu, density)
